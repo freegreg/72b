d3ddeae96dd5f69bca758dd04eae83160e6b17d3
omar.elmohandes90@gmail.com <omar.elmohandes90@gmail.com@87d62ec8-b1a2-8f5d-c6ae-dd81f198c0ec>
2011-04-22 23:31:55 +0000

    Implemented the Engine in the new structure.
    **TODO**
    - Implement UVA like SPOJ.

---
+++ JudgesEngine/UVA.java
@@ -0,0 +1,41 @@
+package Engine;
+
+import java.util.ArrayList;
+/**
+ * @author OmarEl-Mohandes
+ *
+ */
+public class UVA implements Judge{
+
+	@Override
+	public ArrayList<Problem> getAllProblems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Submission getLastSubmission(String coderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Problem getProblemInfo(String problemId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<String> getProblemsSolved(String coderId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void submitProblem(String coderId, String password,
+			String problemId, String language, String code) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
